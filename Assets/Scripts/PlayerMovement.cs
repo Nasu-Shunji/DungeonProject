33c3016839a67941a5bc7b781dd731eafa77062c
Nasu-Shunji <shunjinasu@gmail.com>
2026-07-12 22:33:40 +0900

    Add player rotation

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,9 +2,13 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed = 5f;
+     [SerializeField] private float moveSpeed = 5f;
+     [SerializeField] private float gravity = -9.81f;
+     [SerializeField] private float rotationSpeed = 10f;
+     [SerializeField] private Transform cameraTransform;
 
     private CharacterController controller;
+    private float verticalVelocity;
 
     private void Awake()
     {
@@ -16,7 +20,39 @@ public class PlayerMovement : MonoBehaviour
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
 
-        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
-        controller.Move(direction * moveSpeed * Time.deltaTime);
+        Vector3 cameraForward = cameraTransform.forward;
+        Vector3 cameraRight = cameraTransform.right;
+
+        cameraForward.y = 0f;
+        cameraRight.y = 0f;
+
+        cameraForward.Normalize();
+        cameraRight.Normalize();
+
+        Vector3 horizontalMovement =
+    (cameraForward * vertical + cameraRight * horizontal).normalized;
+
+    if (horizontalMovement.sqrMagnitude > 0.01f)
+    {
+        Quaternion targetRotation =
+            Quaternion.LookRotation(horizontalMovement);
+
+        transform.rotation = Quaternion.Slerp(
+            transform.rotation,
+            targetRotation,
+            rotationSpeed * Time.deltaTime
+        );
+    }
+        if (controller.isGrounded && verticalVelocity < 0f)
+        {
+            verticalVelocity = -2f;
+        }
+
+        verticalVelocity += gravity * Time.deltaTime;
+
+        Vector3 movement = horizontalMovement * moveSpeed;
+        movement.y = verticalVelocity;
+
+        controller.Move(movement * Time.deltaTime);
     }
 }
